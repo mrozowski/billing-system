4ae719ab39b455bb503e48880791ce5c43ede614
Szymon Mrozowski <mrozowskion@gmail.com>
2024-02-15 20:18:11 +0100

    init

---
+++ src/main/java/com/mrozowski/softwarepartnertask/SoftwarePartnerTaskApplication.java
@@ -0,0 +1,13 @@
+package com.mrozowski.softwarepartnertask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwarePartnerTaskApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SoftwarePartnerTaskApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/mrozowski/softwarepartnertask/SoftwarePartnerTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrozowski.softwarepartnertask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftwarePartnerTaskApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
