97cdca39818c5f9a591c056ad4a0dac41d0b0abd
Szymon Mrozowski <mrozowskion@gmail.com>
2024-02-19 11:41:54 +0100

    Fix Hibernate N+1 problem when fetching parents

--- src/main/java/com/mrozowski/softwarepartnertask/adapter/outgoing/JpaParentRepository.java
+++ src/main/java/com/mrozowski/softwarepartnertask/adapter/outgoing/JpaParentRepository.java
@@ -10,8 +10,9 @@ import java.util.Optional;
 @Repository
 interface JpaParentRepository extends JpaRepository<ParentEntity, Long> {
 
+  @Query("SELECT p FROM ParentEntity p LEFT JOIN FETCH p.children c LEFT JOIN FETCH c.school WHERE p.id = :id")
   Optional<ParentEntity> findFirstById(long id);
 
-  @Query("SELECT p FROM ParentEntity p JOIN ChildEntity c ON p.id = c.parent.id WHERE c.school.id = :schoolId")
+  @Query("SELECT DISTINCT p FROM ParentEntity p JOIN FETCH p.children c JOIN FETCH c.school s WHERE s.id = :schoolId")
   List<ParentEntity> findAllBySchoolId(long schoolId);
 }
